9dd76e3a33d24f1538b250b4384b2f404734412b
Meng Xiangzeng <dreamzen@163.com>
2021-11-21 17:09:08 +0800

    workspace

---
+++ add/add_one/src/lib.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add_one(3), 4);
+    }
+}
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ add/adder/src/main.rs
@@ -0,0 +1,7 @@
+use add_one;
+use add_two;
+
+fn main() {
+    println!("{}", add_one::add_one(3));
+    println!("{}", add_two::add_two(3));
+}
